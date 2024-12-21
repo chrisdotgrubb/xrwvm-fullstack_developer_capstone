4b231d5c9351213025d542dfbbe56d460afc732a
Chris Grubb <developer.grubb@gmail.com>
2024-12-21 15:24:07 -0500

    chore: Lint JS

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,11 +7,12 @@ import Dealer from "./components/Dealers/Dealer"
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/login" element={<LoginPanel/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/dealers" element={<Dealers/>}/>
+      <Route path="/dealer/:id" element={<Dealer/>}/>
     </Routes>
   );
 }
+
 export default App;
