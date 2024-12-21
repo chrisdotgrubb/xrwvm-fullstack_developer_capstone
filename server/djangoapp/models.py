4a03ddee2e020788c608a75137ba5e82a5fdc2fa
chrisdotgrubb <developer.grubb@gmail.com>
2024-12-21 15:25:11 -0500

    chore: Lint Python multiple of 4

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,34 +7,34 @@ from django.db import models
 # Create your models here.
 
 class CarMake(models.Model):
-  name = models.CharField(max_length=100)
-  description = models.TextField()
-  
-  # Other fields as needed
-  
-  def __str__(self):
-    return self.name  # Return the name as the string representation
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    # Other fields as needed
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
-  car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-  name = models.CharField(max_length=100)
-  # dealer_id = mode-ls.IntegerField(null=True, blank=True)
-  CAR_TYPES = [
-    ('SEDAN', 'Sedan'),
-    ('SUV', 'SUV'),
-    ('WAGON', 'Wagon'),
-    ('TRUCK', 'Truck'),
-    # Add more choices as required
-  ]
-  type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-  year = models.IntegerField(default=2023,
-                             validators=[
-                               MaxValueValidator(2023),
-                               MinValueValidator(2015)
-                             ])
-  
-  # Other fields as needed
-  
-  def __str__(self):
-    return self.name  # Return the name as the string representation
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    # dealer_id = mode-ls.IntegerField(null=True, blank=True)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+    
+    # Other fields as needed
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
